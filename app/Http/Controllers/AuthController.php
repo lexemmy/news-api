6008293476fc62f0f2a0aee295e53ab5fca60a9c
lex <emmy007.el@gmail.com>
2023-02-22 17:22:03 +0100

    add register route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        Validator::make($request->all(), [
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'min:6'],
+        ])->validate();
+
+        User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        if (!$token = Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Authentication failed'], 401);
+        }
+
+        return response()->json([
+            'token' => $token,
+            'user' => Auth::user()
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'App\Http\Controllers'], function () {
+
+    // Auth Module
+    Route::group(['prefix' => 'auth',], function () {
+        Route::controller(AuthController::class)->group(function () {
+            Route::post('/register', 'register');
+        });
+    });
 });

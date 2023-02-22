9e0118c82788808e6d65b87172eda5b5054b3012
lex <emmy007.el@gmail.com>
2023-02-22 17:08:40 +0100

    add uuid trait to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use App\Traits\UuidTrait;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, UuidTrait;
 
     /**
      * The attributes that are mass assignable.

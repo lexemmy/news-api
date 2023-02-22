ba89a0ec46963e216d6c47b89209eab609d6276b
lex <emmy007.el@gmail.com>
2023-02-22 19:45:46 +0100

    update news model

--- app/Models/News.php
+++ app/Models/News.php
@@ -9,4 +9,10 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory, UuidTrait;
+
+    protected $guarded = ['null'];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
 }

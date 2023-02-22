a7e959460faa79cac86c704874cd5e64c67e556a
lex <emmy007.el@gmail.com>
2023-02-22 17:34:21 +0100

    add uuid to news model

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use App\Traits\UuidTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    use HasFactory;
+    use HasFactory, UuidTrait;
 }
